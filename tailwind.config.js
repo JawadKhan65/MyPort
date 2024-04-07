cdd3e172719f89c1afac26814aae95a23188affa
JawadKhan65 <jawadkhan10322@gmail.com>
2024-04-07 20:53:15 +0500

    finalizing1

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,7 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      transitionProperty: {
-        'height': 'height',
-        'spacing': 'margin, padding',
-      }
+
     },
   },
   plugins: [],
